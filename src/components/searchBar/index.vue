f80e9c69a0092b266393b471beba517afe57c4f6
addonma <745299355@qq.com>
2019-08-21 08:54:03 +0800

    陈小芸二百五

--- src/components/searchBar/index.vue
+++ src/components/searchBar/index.vue
@@ -1,5 +1,110 @@
 <template>
-    <div>
-        
+    <div class="classify-type">
+        <ul class="classify-type-ul">
+            <li @click="active(0)" class="classify-type-ul-li">
+                <i></i>
+                全国
+                <div :style="{display: a==0 ? 'block' : 'none'}" class="classify-type-left">
+                    <ul >
+                        <li>全国</li>
+                        <li v-for="item in fcitys" :key="item.CITYJX" >{{item.JXNAME}}</li>
+                    </ul>
+                </div>
+            </li>
+            <li @click="active(1)" class="classify-type-ul-li">
+                <i></i>
+                全部分类
+                <div :style="{display: a==1 ? 'block' : 'none'}" class="classify-type-left">
+                    <ul>
+                        <li>全部分类</li>
+                        <li v-for="item in typeas" :key="item.PRODUCTTYPEAID">{{item.NAME}}</li>
+                    </ul>
+                </div>
+            </li>
+            <li @click="active(2)" class="classify-type-ul-li">
+                <i></i>
+                全部时间
+                <div :style="{display: a==2 ? 'block' : 'none'}" class="classify-type-right">
+                    <ul>
+                        <li>全部时间</li>
+                        <li></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
+<script>
+
+export default {
+    name: 'searchBar',
+    data() {
+          return {
+            a: 9,
+          };
+        },
+    props:{
+        fcitys:Array,
+        typeas:Array
+    },
+    methods: {
+       active(a){
+           return this.a=a;
+       }
+    },
+    
+}
+</script>
+<style lang="scss">
+    .classify-type{
+        height: 46px;
+        width: 100%;
+        margin-bottom: 20px;
+        .classify-type-ul{
+            width:100%;
+            height: 100%;
+            display: flex;
+            border-bottom: 1px solid #f9fafa;
+            box-sizing: border-box;
+            .classify-type-ul-li{
+                flex: 1;
+                text-align: center;
+                line-height: 45px;
+                color: #999ea3;
+                position: relative;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                overflow: hidden;
+                padding: 0 20px 0 10px;
+            }
+            .classify-type-left {
+                position: fixed;
+                width: 100%;
+                height: calc(100% - 76px);
+                left: 0;
+                z-index: 100;
+                background: rgba(0, 0, 0, .5);
+                overflow: auto;
+                ul{
+                    width:100%;
+                    padding: 20px 0;
+                    background: #fff;
+                    // height: 1000px;
+                    border-radius: 0 0 20px 20px;
+                    zoom:1;
+                    overflow: hidden;
+                    li{
+                        height: 45px;
+                        line-height: 45px;
+                        text-align: center;
+                        float: left;
+                        width: 33.33%;
+                        color:#000;
+                        // background: #fff;
+                    }
+                }
+            }
+        }
+        
+    }
+</style>
--- src/store/modules/show.js
+++ src/store/modules/show.js
@@ -4,37 +4,109 @@ import request from '../../utils/request'
 export default {
   namespaced: true,
   state: {
-    pagerMemoryList:[],
-    pbgimgs:[],
-    pbgimgsList:[]
+    pagerMemoryList: [],
+    pbgimgs: [],
+    pbgimgsList: [],
+    num: 1,
+    fcitys: [],
+    typeas: []
   },
-  getters: {
-    
-  },
+  getters: {},
   mutations: {
     setPagerMemoryList(state, payload) {
       state.pagerMemoryList = payload.pagerMemorys
+      // console.log(state.pagerMemoryList)
       state.pbgimgs = payload.pagerMemorys.map(item => item.pbigimg)
-      state.pbgimgsList=state.pbgimgs.map(item=>'//static.228.cn'+item)
+      // console.log(state.pbgimgs)
+      state.pbgimgsList = state.pbgimgs.map(item => '//static.228.cn' + item)
       // console.log(payload.pagerMemorys)
-      // state.total = payload.total
+      state.fcitys = payload.allData.fcitys
+      state.typeas = payload.allData.typeas
+      console.log(state.fcitys)
+      console.log(state.typeas)
+    },
+    setMorePagerMemorys(state, payload) {
+      state.pagerMemoryList = payload.MorePagerMemorys
+      state.pbgimgs = state.pagerMemoryList.map(item => item.pbigimg)
+      state.pbgimgsList = state.pbgimgs.map(item => '//static.228.cn' + item)
+      
     }
-
   },
   actions: {
-
-    getShowList({commit}) {
+    //首次数据获取----------------------------------------------------------------------->
+    getShowList({ commit }) {
       setTimeout(() => {
         request
-          .get('http://localhost:15590/api/server/category/default.json')
+          .get('http://localhost:24635/api/server/category/default.json')
           .then(res => {
             // console.log(res)
             // console.log(res.data.pagerMemoryList)
             if (res.result.code == 1) {
               commit({
                 type: 'setPagerMemoryList',
-                pagerMemorys: res.data.pagerMemoryList
-                
+                pagerMemorys: res.data.pagerMemoryList,
+                allData: res.data
+              })
+            }
+          })
+      }, 500)
+    },
+    //点击获取更多，数据获取------------------------------------------------------------->
+    getMoreList({ commit, state }) {
+      state.num++
+      setTimeout(() => {
+        request
+          .post(
+            'http://localhost:24635/api/server/search/moreSearch.json',
+            {
+              //请求方法1
+              params: {
+                datestrEscape: 'all',
+                datestr: 'all',
+                type: 1,
+                tagstr: 'all',
+                datestrSelected: 'all',
+                typebjx: 'all',
+                orderstype: 'all',
+                filter: 'all',
+                total: 1729,
+                typeajx: 'all',
+                cityjx: 'all',
+                keyword: '',
+                pagenum: state.num
+              }
+              //  请求参数做修改，方法2
+              // params: datestrEscape=all&datestr=all&type=1&tagstr=all&datestrSelected=all&typebjx=all&orderstype=all&filter=all&total=1729&typeajx=all&cityjx=all&keyword=&pagenum=2'}, {
+            },
+            {
+              transformRequest: data => {
+                let result = []
+                for (let key in data) {
+                  let a = data[key]
+                  let arr = []
+                  for (let i in a) {
+                    arr.push(`${i}=${a[i]}`)
+                  }
+                  result.push(`${key}=${encodeURIComponent(arr.join('&'))}`)
+                }
+                return result.join('')
+              }
+              // 请求参数做修改，方法2
+              // transformRequest: (data) => {
+              //   let arr = [];
+              //   for (let key in data) {
+              //     arr.push(`${key}=${data[key]}`)
+              //   }
+              //   return arr.join('&')
+            }
+          )
+          .then(res => {
+            console.log(res.result.code)
+            console.log(res.data.pagerMemoryList)
+            if (res.result.code == 1) {
+              commit({
+                type: 'setMorePagerMemorys',
+                MorePagerMemorys: state.pagerMemoryList.concat(res.data.pagerMemoryList),
               })
             }
           })
@@ -42,4 +114,3 @@ export default {
     }
   }
 }
-
